8dd329ad053837c5ccd795be89aa1d9cc9b5e56e
Mdyeahia <mdyeahia.khan@gmail.com>
2023-01-05 18:17:34 +0600

    machine production entry

--- src/app/machine/machine.component.ts
+++ src/app/machine/machine.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormBuilder } from '@angular/forms';
 import { from } from 'rxjs';
 
 @Component({
@@ -7,9 +8,21 @@ import { from } from 'rxjs';
   styleUrls: ['./machine.component.css']
 })
 export class MachineComponent {
+  constructor(private _fb: FormBuilder) {}
   ngOnInit(): void {
+    this.frmppEntry = this._fb.group({  
+      productionDate: "",
+      ppNo:"",
+      fabricType:"",
+      yarnCount:"",
+      lotNo:"",
+      slNo:"",
+      orderQty:""
+
+  });
     this.Allmachine();
   }
+  frmppEntry:any= FormGroup;
   machineList: any;
   sData: any;
   scheduleData: any = [];
@@ -18,6 +31,7 @@ export class MachineComponent {
   toggle:any;
   dragableObj :any={}
   displayStyle:any = "none";
+  ppEntryStyle:any = "none";
  // dragableObj :any={}
  detailsData:any={}
 
@@ -28,10 +42,12 @@ export class MachineComponent {
         "Dia": 30,
         "GG": 24,
         "data": [{
-          "pp": "020/21E",
+          "ppNo": "020/21E",
           "fabricType": "Cotton",
-          "sl": "0.2",
-          "yarnCode": "V-231"
+          "yarnCount":"29s",
+          "lotNo":"v-270",
+          "slNo": "2.9",
+          "orderQty": "10kg"
         }]
       },
 
@@ -46,10 +62,12 @@ export class MachineComponent {
         "Dia": 30,
         "GG": 24,
         "data": [{
-          "pp": "001/22C",
-          "fabricType": "Siro",
-          "sl": "0.2",
-          "yarnCode": "V-231"
+          "ppNo": "001/22C",
+          "fabricType": "(1*1) sp rib)",
+          "yarnCount":"28s/wl+20D F.F",
+          "lotNo":"v-380",
+          "slNo": "2.9",
+          "orderQty": "20kg"
         }]
       },
     ]
@@ -63,7 +81,7 @@ export class MachineComponent {
   onDropTo(event: any, data: any) {
   
     if(data.data.length == 0){
-      data.data.push(this.dragableObj.data);
+      data.data.push(this.dragableObj.data[0]);
 
       this.dragableObj.data=[];
 
@@ -84,7 +102,8 @@ export class MachineComponent {
     this.displayStyle = "block";
     
     
-     this.detailsData=data==undefined?{}:data
+     this.detailsData=data
+     console.log(this.detailsData.data.length)
      if(data==undefined){this.displayStyle = "none";}
    
     
@@ -92,6 +111,26 @@ export class MachineComponent {
   closePopup() {
     this.displayStyle = "none";
     this.detailsData={}
+  }
+  ppEntry(data:any){
+    if(data.data.length == 0){
+    this.displayStyle = "none";
+    this.ppEntryStyle="block";
+    }
+    this.displayStyle = "none";
+  }
+  closeppEntry(){
+    this.displayStyle = "none";
+    this.ppEntryStyle="none";
+  }
+
+
+  SavePP(data:any){
+    console.log(data)
+    console.log(this.machineList)
+
+    this.machineList[1].data.push(data)
+
   }
 
 }
