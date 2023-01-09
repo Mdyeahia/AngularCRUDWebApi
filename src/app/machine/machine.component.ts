561250025db7e9eaedcd70f664ccfc8b5a77b11a
Mdyeahia <mdyeahia.khan@gmail.com>
2023-01-09 18:33:48 +0600

    machine dashboard with todoList,drag and drop

--- src/app/machine/machine.component.ts
+++ src/app/machine/machine.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup,FormBuilder } from '@angular/forms';
-import { from } from 'rxjs';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { flatMap, from } from 'rxjs';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-machine',
@@ -8,32 +9,35 @@ import { from } from 'rxjs';
   styleUrls: ['./machine.component.css']
 })
 export class MachineComponent {
-  constructor(private _fb: FormBuilder) {}
+  constructor(private _fb: FormBuilder) { }
   ngOnInit(): void {
-    this.frmppEntry = this._fb.group({  
+    this.frmppEntry = this._fb.group({
       productionDate: "",
-      ppNo:"",
-      fabricType:"",
-      yarnCount:"",
-      lotNo:"",
-      slNo:"",
-      orderQty:""
+      ppNo: "",
+      fabricType: "",
+      yarnCount: "",
+      lotNo: "",
+      slNo: "",
+      orderQty: ""
 
-  });
+    });
     this.Allmachine();
+    this.pp_production();
+
   }
-  frmppEntry:any= FormGroup;
+  frmppEntry: any = FormGroup;
   machineList: any;
+  pp_production_list: any;
   sData: any;
   scheduleData: any = [];
   tData: any;
   trailerData: any = [];
-  toggle:any;
-  dragableObj :any={}
-  displayStyle:any = "none";
-  ppEntryStyle:any = "none";
- // dragableObj :any={}
- detailsData:any={}
+  toggle: any;
+  dragableObj: any = {}
+  displayProgress: any = "none";
+  ppEntryStyle: any = "none";
+  // dragableObj :any={}
+  detailsData: any = []
 
   Allmachine() {
     this.machineList = [
@@ -44,46 +48,142 @@ export class MachineComponent {
         "data": [{
           "ppNo": "020/21E",
           "fabricType": "Cotton",
-          "yarnCount":"29s",
-          "lotNo":"v-270",
+          "yarnCount": "29s",
+          "lotNo": "v-270",
           "slNo": "2.9",
           "orderQty": "10kg"
-        }]
+        },
+        {
+          "ppNo": "050/11E",
+          "fabricType": "Siro",
+          "yarnCount": "29s",
+          "lotNo": "v-270",
+          "slNo": "2.9",
+          "orderQty": "10kg"
+        }
+        ]
       },
 
       {
-        "No": 15,
+        "No": 16,
         "Dia": 30,
         "GG": 24,
         "data": []
       },
       {
-        "No": 16,
+        "No": 17,
+        "Dia": 30,
+        "GG": 24,
+        "data": []
+      },
+      {
+        "No": 18,
+        "Dia": 30,
+        "GG": 24,
+        "data": []
+      },
+      {
+        "No": 19,
+        "Dia": 30,
+        "GG": 24,
+        "data": []
+      },
+      {
+        "No": 20,
+        "Dia": 30,
+        "GG": 24,
+        "data": []
+      },
+      {
+        "No": 21,
+        "Dia": 30,
+        "GG": 24,
+        "data": []
+      },
+      {
+        "No": 22,
+        "Dia": 30,
+        "GG": 24,
+        "data": []
+      },
+      {
+        "No": 23,
+        "Dia": 30,
+        "GG": 24,
+        "data": []
+      },
+      {
+        "No": 24,
         "Dia": 30,
         "GG": 24,
         "data": [{
           "ppNo": "001/22C",
           "fabricType": "(1*1) sp rib)",
-          "yarnCount":"28s/wl+20D F.F",
-          "lotNo":"v-380",
+          "yarnCount": "28s/wl+20D F.F",
+          "lotNo": "v-380",
           "slNo": "2.9",
           "orderQty": "20kg"
         }]
       },
     ]
   }
-  
+
+  pp_production() {
+    this.pp_production_list = [
+      {
+        "machineNo": 14,
+        "ppNo": "020/21E",
+        "fabricType": "Cotton",
+        "yarnCount": "29s",
+        "lotNo": "v-270",
+        "slNo": "2.9",
+        "orderQty": "10kg",
+        "status":"Added"
+      },
+      {
+        "machineNo": 14,
+        "ppNo": "021/22E",
+        "fabricType": "Siro",
+        "yarnCount": "29s",
+        "lotNo": "v-270",
+        "slNo": "2.9",
+        "orderQty": "10kg",
+        "status":"Added"
+      },
+      {
+        "machineNo": 14,
+        "ppNo": "024/20E",
+        "fabricType": "Cotton",
+        "yarnCount": "29s",
+        "lotNo": "v-270",
+        "slNo": "2.9",
+        "orderQty": "10kg",
+        "status":"Active"
+      },
+      {
+        "machineNo": 24,
+        "ppNo": "024/20E",
+        "fabricType": "Melange",
+        "yarnCount": "29s",
+        "lotNo": "v-270",
+        "slNo": "2.9",
+        "orderQty": "10kg",
+        "status":"Active"
+      },
+    ]
+  }
+
 
   dragStartFrom(event: any, data: any) {
-    this.dragableObj =  data;
+    this.dragableObj = data;
   }
 
   onDropTo(event: any, data: any) {
-  
-    if(data.data.length == 0){
+
+    if (data.data.length == 0) {
       data.data.push(this.dragableObj.data[0]);
 
-      this.dragableObj.data=[];
+      this.dragableObj.data = [];
 
       console.log(this.machineList)
     }
@@ -96,36 +196,49 @@ export class MachineComponent {
     event.stopPropagation();
     event.preventDefault();
   }
-  
-  
-  openPopup(data:any) {
-    this.displayStyle = "block";
-    
-    
-     this.detailsData=data
-     console.log(this.detailsData.data.length)
-     if(data==undefined){this.displayStyle = "none";}
-   
-    
+
+  drop(event: CdkDragDrop<string[]>) {
+    if(this.detailsData[event.currentIndex].status !="Active")
+    {
+      moveItemInArray(this.detailsData, event.previousIndex, event.currentIndex);
+    }
+    else(
+      alert('Stop the running Production!!!')
+    )
+    console.log(event.previousIndex,     event.currentIndex )
+  }
+  openProgress(data: any) {
+    this.displayProgress = "block";
+    for(var i=0;i<this.pp_production_list.length;i++)
+    {
+      if(this.pp_production_list[i].machineNo==data)
+      {
+        this.detailsData.push(this.pp_production_list[i]);
+      }
+    }
+
+    console.log(this.detailsData)
+    if (data == undefined) { this.displayProgress = "none"; }
+
+
   }
   closePopup() {
-    this.displayStyle = "none";
-    this.detailsData={}
+    this.displayProgress = "none";
+    this.detailsData = []
   }
-  ppEntry(data:any){
-    if(data.data.length == 0){
-    this.displayStyle = "none";
-    this.ppEntryStyle="block";
-    }
-    this.displayStyle = "none";
+  ppEntry(data: any) {
+
+    this.displayProgress = "none";
+    this.ppEntryStyle = "block";
+
   }
-  closeppEntry(){
-    this.displayStyle = "none";
-    this.ppEntryStyle="none";
+  closeppEntry() {
+    this.displayProgress = "none";
+    this.ppEntryStyle = "none";
   }
 
 
-  SavePP(data:any){
+  SavePP(data: any) {
     console.log(data)
     console.log(this.machineList)
 
