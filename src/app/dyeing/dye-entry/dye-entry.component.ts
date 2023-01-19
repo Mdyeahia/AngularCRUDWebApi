c77c102966974c5aef4b6c601116f73763367457
Mdyeahia <mdyeahia.khan@gmail.com>
2023-01-19 14:40:18 +0600

    DyePLan entry and list page

--- src/app/dyeing/dye-entry/dye-entry.component.ts
+++ src/app/dyeing/dye-entry/dye-entry.component.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { dyeingpp } from '../../model/dyeingPP.model';
 @Component({
   selector: 'app-dye-entry',
   templateUrl: './dye-entry.component.html',
   styleUrls: ['./dye-entry.component.css']
 })
+@Injectable({
+  providedIn: 'root',
+})
 export class DyeEntryComponent {
   constructor() { }
-  dyeingMachineList:any = [];
-  machineNo:any
-  capacity:any
-  execution_date:any
-  totalQty:any
+  dyeingMachineList: any = [];
+  machineNo: any
+  capacity: any
+  execution_date: any
+  totalQty: any
   pp_dyeEntry_List: any = [];
-  dyeplan_list:any=[]
-  dyeingpp=new dyeingpp()
+  dyeplan_list: any = []
+  dyeingpp = new dyeingpp()
 
   ngOnInit(): void {
     this.AllDyeingMachineList();
-    this.dyeingpp=new dyeingpp();
+    this.dyeingpp = new dyeingpp();
     this.pp_dyeEntry_List.push(this.dyeingpp);
 
 
@@ -51,35 +54,38 @@ export class DyeEntryComponent {
       }
     ]
   }
-  setCapacity(data:any){
+  setCapacity(data: any) {
 
-    for(var i=0;i<this.dyeingMachineList.length;i++)
-    {
-      if(this.dyeingMachineList[i].no==data)
-      {
-        this.capacity=this.dyeingMachineList[i].capacity
+    for (var i = 0; i < this.dyeingMachineList.length; i++) {
+      if (this.dyeingMachineList[i].no == data) {
+        this.capacity = this.dyeingMachineList[i].capacity
       }
     }
-    this.dyeingpp.mNo=data;
-    this.machineNo=data;
+    this.dyeingpp.mNo = data;
+    this.machineNo = data;
 
   }
   addForm() {
-    this.dyeingpp=new dyeingpp();
-    this.dyeingpp.mNo=this.machineNo;
+    this.dyeingpp = new dyeingpp();
+    this.dyeingpp.mNo = this.machineNo;
     this.pp_dyeEntry_List.push(this.dyeingpp);
 
   }
-  removeitem(i:any){
+  removeitem(i: any) {
     this.pp_dyeEntry_List.splice(i);
   }
-  onsubmit(){
-    this.dyeplan_list=[]
-    for(var i=0;i<this.pp_dyeEntry_List.length;i++)
-    {
-     this.dyeplan_list.push(this.pp_dyeEntry_List[i])
+  onsubmit() {
+    this.dyeplan_list = []
+    for (var i = 0; i < this.pp_dyeEntry_List.length; i++) {
+      this.dyeplan_list.push(this.pp_dyeEntry_List[i])
     }
 
-     console.log(this.dyeplan_list)
-   }
+    localStorage.setItem('dyePP', JSON.stringify(this.dyeplan_list))
+
+    
+  }
+
+  dyeEntryData(){
+    return localStorage.getItem('dyePP')
+  }
 }
--- src/app/dyeing/dyeing-list/dyeing-list.component.ts
+++ src/app/dyeing/dyeing-list/dyeing-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DyeEntryComponent } from '../dye-entry/dye-entry.component'; 
 @Component({
   selector: 'app-dyeing-list',
   templateUrl: './dyeing-list.component.html',
   styleUrls: ['./dyeing-list.component.css']
 })
-export class DyeingListComponent {
+export class DyeingListComponent implements OnInit {
 
+  constructor(private sharedService: DyeEntryComponent) { }
+
+  ngOnInit() {
+ 
+    let data:any =this.sharedService.dyeEntryData()
+    this.dyeplanList=JSON.parse(data)
+    console.log(this.dyeplanList)
+  }
+  dyeplanList:any
+
+  // ngAfterViewInit() {
+  //   let data:any =this.sharedService.dyeEntryData()
+  //   this.dyeplanList=JSON.parse(data)
+  //   console.log(this.dyeplanList)
+  // }
 }
