a2932a21dafd4233a818d8f7380bfc73f0be2509
Mdyeahia <mdyeahia.khan@gmail.com>
2023-01-21 15:58:12 +0600

    ag grid implemented

--- src/app/dyeing/dyeing-list/dyeing-list.component.ts
+++ src/app/dyeing/dyeing-list/dyeing-list.component.ts
@@ -1,21 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
 import { DyeEntryComponent } from '../dye-entry/dye-entry.component'; 
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-dyeing-list',
   templateUrl: './dyeing-list.component.html',
   styleUrls: ['./dyeing-list.component.css']
 })
-export class DyeingListComponent implements OnInit {
+export class DyeingListComponent {
+
+
+
+
+ // Each Column Definition results in one Column.
+ public columnDefs: ColDef[] = [
+  { field: 'mNo'},
+  { field: 'ppNo'},
+  { field: 'buyerName'},
+  { field: 'color' },
+  { field: 'quantity' }
+
+];
+
+// DefaultColDef sets props common to all Columns
+public defaultColDef: ColDef = {
+  sortable: true,
+  filter: true,
+  editable:true
+};
+
+// Data that gets displayed in the grid
+rowData :any
 
   constructor(private sharedService: DyeEntryComponent) { }
-
-  ngOnInit() {
  
+  ngOnInit() {
     let data:any =this.sharedService.dyeEntryData()
-    this.dyeplanList=JSON.parse(data)
-    console.log(this.dyeplanList)
+    this.rowData = JSON.parse(data)
+    console.log(this.rowData)
   }
-  dyeplanList:any
+
+
+
+  // constructor(private sharedService: DyeEntryComponent) { }
+
+  // ngOnInit() {
+ 
+  //   let data:any =this.sharedService.dyeEntryData()
+  //   this.dyeplanList=JSON.parse(data)
+  //   console.log(this.dyeplanList)
+  // }
+  // dyeplanList:any
 
   // ngAfterViewInit() {
   //   let data:any =this.sharedService.dyeEntryData()
